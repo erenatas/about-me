3f9da371a2da41e6c2c6ea3b3a8eed9ca2340aa6
Eren Atas <eren_atas@hotmail.com>
2024-12-05 22:09:36 +0100

    Add profiling, tracing, pyroscope, tempo
    Signed-off-by: Eren Atas <eren_atas@hotmail.com>

--- src/resume.rs
+++ src/resume.rs
@@ -19,28 +19,37 @@ pub fn Resume() -> impl IntoView {
 pub async fn get_resume_svg() -> Result<String, ServerFnError> {
     use crate::typst::lib::TypstBuilder;
     use tracing::info;
+    use opentelemetry::global;
+    use opentelemetry::trace::Tracer;
+    
+    let tracer: global::BoxedTracer = global::tracer("get_resume_svg");
+    let out = tracer.in_span("call_api", |_cx| -> Result<String, ServerFnError> {
+        info!("Generating resume..");
+        let current_dir = std::env::current_dir()
+            .map_err(|e| ServerFnError::new(e))?;
 
-    // Get the current directory and construct absolute paths
-    info!("Generating resume..");
-    let current_dir = std::env::current_dir()?;
-    let font_path = current_dir
-        .join("resources")
-        .join("typst")
-        .join("NewCMMath-Regular.otf")
-        .display()
-        .to_string();
+        let font_path = current_dir
+            .join("resources")
+            .join("typst")
+            .join("NewCMMath-Regular.otf")
+            .display()
+            .to_string();
 
-    let file_path = current_dir
-        .join("resources")
-        .join("typst")
-        .join("main.typ")
-        .display()
-        .to_string();
-    let font_data: Vec<u8> = std::fs::read(&font_path)?;
-    let file_data = std::fs::read_to_string(&file_path)?;
-    let mut typst_builder = TypstBuilder::new(&file_data, &font_data);
+        let file_path = current_dir
+            .join("resources")
+            .join("typst")
+            .join("main.typ")
+            .display()
+            .to_string();
 
-    let svg = typst_builder.generate_svg();
+        let font_data = std::fs::read(&font_path)
+            .map_err(|e| ServerFnError::new(e))?;
+        let file_data = std::fs::read_to_string(&file_path)
+            .map_err(|e| ServerFnError::new(e))?;
+        
+        let mut typst_builder = TypstBuilder::new(&file_data, &font_data);
+        Ok(typst_builder.generate_svg())
+    })?;
 
-    Ok(svg)
+    Ok(out)
 }

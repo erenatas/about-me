92ab34c79805903d93e4febc9983076a2892e06c
Eren Atas <eren_atas@hotmail.com>
2024-12-06 21:14:24 +0100

    Fix axum-otel-metrics configuration, update compose
    Signed-off-by: Eren Atas <eren_atas@hotmail.com>

--- src/observability/lib.rs
+++ src/observability/lib.rs
@@ -1,4 +1,3 @@
-
 #[cfg(feature = "ssr")]
 use axum_otel_metrics::HttpMetricsLayer;
 #[cfg(feature = "ssr")]
@@ -8,51 +7,58 @@ use opentelemetry::trace::TraceError;
 #[cfg(feature = "ssr")]
 use opentelemetry_sdk::logs::{LogError, LoggerProvider};
 #[cfg(feature = "ssr")]
-use opentelemetry_sdk::{metrics::{MetricError, SdkMeterProvider}, Resource, runtime, trace as sdktrace};
+use opentelemetry_sdk::{
+    metrics::{MetricError, SdkMeterProvider},
+    runtime, trace as sdktrace, Resource,
+};
 #[cfg(feature = "ssr")]
 use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
 #[cfg(feature = "ssr")]
 use pyroscope::pyroscope::PyroscopeAgentReady;
 #[cfg(feature = "ssr")]
-use pyroscope::PyroscopeError;
-#[cfg(feature = "ssr")]
 use pyroscope::PyroscopeAgent;
+#[cfg(feature = "ssr")]
+use pyroscope::PyroscopeError;
 
+#[cfg(feature = "ssr")]
+const ROLE_NAME: &str = "about-me";
+
+#[cfg(feature = "ssr")]
+const OTLP_ENDPOINT: &str = "http://alloy:4317";
+#[cfg(feature = "ssr")]
+const PYROSCOPE_ENDPOINT: &str = "http://alloy:9999";
 
 #[cfg(feature = "ssr")]
 static RESOURCE: Lazy<Resource> = Lazy::new(|| {
     use opentelemetry::KeyValue;
-    Resource::new(vec![KeyValue::new(SERVICE_NAME, "about-me")])
+    Resource::new(vec![KeyValue::new(SERVICE_NAME, ROLE_NAME)])
 });
 
 #[cfg(feature = "ssr")]
-fn init_otel_logging()  -> Result<LoggerProvider, LogError> {
-    use opentelemetry_otlp::WithExportConfig;
+fn init_otel_logging() -> Result<LoggerProvider, LogError> {
     use opentelemetry_otlp::LogExporter;
+    use opentelemetry_otlp::WithExportConfig;
     use opentelemetry_sdk::runtime;
-
-    const OTLP_ENDPOINT: &str = "http://127.0.0.1:4317";
 
     let exporter = LogExporter::builder()
         .with_tonic()
         .with_endpoint(OTLP_ENDPOINT)
         .with_protocol(opentelemetry_otlp::Protocol::Grpc)
         .build()?;
-    
+
     Ok(LoggerProvider::builder()
         .with_resource(RESOURCE.clone())
         .with_batch_exporter(exporter, runtime::Tokio)
-        .build()
-    )
+        .build())
 }
 
 #[cfg(feature = "ssr")]
 fn init_tracer_provider() -> Result<sdktrace::TracerProvider, TraceError> {
-    use opentelemetry_otlp::{WithExportConfig, SpanExporter};
-    
+    use opentelemetry_otlp::{SpanExporter, WithExportConfig};
+
     let exporter = SpanExporter::builder()
         .with_tonic()
-        .with_endpoint("http://localhost:4317")
+        .with_endpoint(OTLP_ENDPOINT)
         .build()?;
     Ok(sdktrace::TracerProvider::builder()
         .with_resource(RESOURCE.clone())
@@ -69,41 +75,42 @@ fn init_otel_metrics() -> Result<SdkMeterProvider, MetricError> {
 
     let reader = PeriodicReader::builder(exporter, runtime::Tokio).build();
 
-    Ok(SdkMeterProvider::builder().with_reader(reader).with_resource(RESOURCE.clone()).build())
+    Ok(SdkMeterProvider::builder()
+        .with_reader(reader)
+        .with_resource(RESOURCE.clone())
+        .build())
 }
 
 #[cfg(feature = "ssr")]
 pub fn init_pyroscope() -> Result<PyroscopeAgent<PyroscopeAgentReady>, PyroscopeError> {
     use pyroscope::PyroscopeAgent;
-    use pyroscope_pprofrs::PprofConfig;
     use pyroscope_pprofrs::pprof_backend;
+    use pyroscope_pprofrs::PprofConfig;
 
     // Configure profiling backend
     let pprof_config = PprofConfig::new().sample_rate(100);
     let backend_impl = pprof_backend(pprof_config);
 
     // Configure Pyroscope Agent
-    PyroscopeAgent::builder("http://localhost:9999", "about-me")
-    .backend(backend_impl)
-    .build()
+    PyroscopeAgent::builder(PYROSCOPE_ENDPOINT, ROLE_NAME)
+        .backend(backend_impl)
+        .build()
 }
 
 #[cfg(feature = "ssr")]
 pub fn init_opentelemetry() {
-    use opentelemetry::global;
-    use tracing_subscriber::{EnvFilter, prelude::*, Registry};
     use log::LevelFilter;
-    use tracing::subscriber::set_global_default;
+    use opentelemetry::global;
     use opentelemetry_appender_log::OpenTelemetryLogBridge;
     use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
-    
+    use tracing::subscriber::set_global_default;
+    use tracing_subscriber::{prelude::*, EnvFilter, Registry};
 
     match init_otel_logging() {
         Ok(logger_provider) => {
             // Create the OpenTelemetry bridges
             let otel_log_bridge = OpenTelemetryLogBridge::new(&logger_provider);
             let otel_layer = OpenTelemetryTracingBridge::new(&logger_provider);
-
 
             // Create an EnvFilter that only allows INFO and above
             let filter = EnvFilter::new("info");
@@ -115,14 +122,12 @@ pub fn init_opentelemetry() {
                 .with(otel_layer);
 
             // Set the global subscriber
-            set_global_default(subscriber)
-                .expect("Failed to set global subscriber");
+            set_global_default(subscriber).expect("Failed to set global subscriber");
 
             // Set up the log bridge after tracing is initialized
-            log::set_boxed_logger(Box::new(otel_log_bridge))
-                .expect("Failed to set logger");
+            log::set_boxed_logger(Box::new(otel_log_bridge)).expect("Failed to set logger");
             log::set_max_level(LevelFilter::Info);
-        },
+        }
         Err(error) => {
             eprintln!("Failed to set the logger: {}", error);
         }
@@ -130,7 +135,7 @@ pub fn init_opentelemetry() {
     match init_otel_metrics() {
         Ok(meter_provider) => {
             global::set_meter_provider(meter_provider);
-        },
+        }
         Err(error) => {
             eprintln!("Failed to set the meter: {}", error);
         }
@@ -140,7 +145,7 @@ pub fn init_opentelemetry() {
         Ok(tracer_provider) => {
             global::set_tracer_provider(tracer_provider);
             println!("Tracer has been set successfully.")
-        },
+        }
         Err(error) => {
             eprintln!("Failed to set the tracer: {}", error)
         }
@@ -152,8 +157,8 @@ pub fn get_axum_metrics_layer() -> HttpMetricsLayer {
     use axum_otel_metrics::HttpMetricsLayerBuilder;
 
     HttpMetricsLayerBuilder::new()
-    .with_service_name("about-me".to_string())
-    .with_service_version(env!("CARGO_PKG_VERSION").to_string())
-    .with_exporter("otlp".to_string())
-    .build()
+        .with_service_name(ROLE_NAME.to_string())
+        .with_service_version(env!("CARGO_PKG_VERSION").to_string())
+        .with_exporter("otlp".to_string())
+        .build()
 }
--- src/resume.rs
+++ src/resume.rs
@@ -18,15 +18,14 @@ pub fn Resume() -> impl IntoView {
 #[server]
 pub async fn get_resume_svg() -> Result<String, ServerFnError> {
     use crate::typst::lib::TypstBuilder;
-    use tracing::info;
     use opentelemetry::global;
     use opentelemetry::trace::Tracer;
-    
+    use tracing::info;
+
     let tracer: global::BoxedTracer = global::tracer("get_resume_svg");
     let out = tracer.in_span("call_api", |_cx| -> Result<String, ServerFnError> {
         info!("Generating resume..");
-        let current_dir = std::env::current_dir()
-            .map_err(|e| ServerFnError::new(e))?;
+        let current_dir = std::env::current_dir().map_err(|e| ServerFnError::new(e))?;
 
         let font_path = current_dir
             .join("resources")
@@ -42,11 +41,9 @@ pub async fn get_resume_svg() -> Result<String, ServerFnError> {
             .display()
             .to_string();
 
-        let font_data = std::fs::read(&font_path)
-            .map_err(|e| ServerFnError::new(e))?;
-        let file_data = std::fs::read_to_string(&file_path)
-            .map_err(|e| ServerFnError::new(e))?;
-        
+        let font_data = std::fs::read(&font_path).map_err(|e| ServerFnError::new(e))?;
+        let file_data = std::fs::read_to_string(&file_path).map_err(|e| ServerFnError::new(e))?;
+
         let mut typst_builder = TypstBuilder::new(&file_data, &font_data);
         Ok(typst_builder.generate_svg())
     })?;
